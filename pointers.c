0effce60fe5a7556c02c0cd192884a55af8d1077
ppraneeth058 <55269851+ppraneeth058@users.noreply.github.com>
2019-09-13 11:44:37 +0530

    Add files via upload

---
+++ pointers.c
@@ -0,0 +1,68 @@
+// C Programme to demonstrate pointers
+#include<stdio.h>
+void main()
+{
+        int *p;
+       int *p;
+int *p;
+int *p;
+int *p;
+int *p;
+int *p;
+int *p;
+int *p;
+int *p;
+int *p;
+int *p;
+int *p;
+int *p;
+int *p;
+int *p;
+int *p;
+int *p;
+int *p;
+int *p;
+int *p;
+int *p;
+int *p;
+int *p;
+int *p;
+int *p;
+int *p;
+int *p;
+int *p;
+int *p;
+int *p;
+int *p;
+int *p;
+int *p;
+int *p;
+int *p;
+int *p;
+int *p;
+int *p;
+int *p;
+int *p;
+int *p;
+int *p;
+int *p;
+int *p;
+int *p;
+int *p;
+int *p;
+int *p;
+int *p;
+int *p;
+int *p;
+int *p;
+int *p;
+int *p;
+int *p;
+int *p;
+int *p;
+vpid
+
+
+}
+
+
